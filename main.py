7948f0b83b54b28a72aa19648692779c02287f0e
Aezore <silverdisc@gmail.com>
2015-02-17 16:42:18 +0100

    Fixed button refresh and time fetching - kv lang file added

--- main.py
+++ main.py
@@ -7,35 +7,55 @@ import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.clock import Clock
-server_address = ('192.168.0.196', 55555)
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
-socket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('192.168.0.193', 55555)
+raspi_address = ('192.168.0.197', 55554)
+
+socket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # bind server
 socket1.bind(server_address)
-data, addr = socket1.recvfrom(1024)
-strvalue = data.decode('utf-8')
+
+socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create raspi send command bind
 
 
-class TimeFetch(GridLayout):
+# data, addr = socket1.recvfrom(1024)
+#strvalue = data.decode('utf-8')
 
-    def etiqueta(self, **kwargs):
-        super(TimeFetch, self).__init__(**kwargs)
-        self.cols = 1
-        self.tiempo = Label(text='tiempo ' + strvalue)
-        self.add_widget(self.tiempo)
+
+class TimeFetch(BoxLayout):
+
+    #text_label = StringProperty("Tiempo:")
+
+    # def __init__(self, **kwargs):
+    #     super(TimeFetch, self).__init__(**kwargs)
+    #
+    #     self.cols = 2
+    #
+    #     tiempo = Label(text=text_label)
+    #     self.add_widget(tiempo)
+    #
+    #     boton = Button(text="Fetch!")
+    #     boton.bind(on_press=self.on_event)
+    #     self.add_widget(boton)
+
+    def on_event(self, *args):
+        print("Fetching time: ")
+        comando = 20
+        socket2.sendto(bytes(comando), raspi_address)
+        data, addr = socket1.recvfrom(1024)
+        label = self.ids.time_label
+        label.text = str(data)
 
 
 class RigMK2core(App):
-
-    def recv_mesg(self):
-        TimeFetch.tiempo.text += 'tiempo ' + strvalue
-
     def build(self):
-        tiempo = TimeFetch()
-        Clock.schedule_interval(tiempo.etiqueta, 1.0 / 60.0)
-        return tiempo
+        return TimeFetch()
+        #tiempo = TimeFetch()
+        #return tiempo
 
 
 if __name__ == '__main__':
