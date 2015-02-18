a564d655d13fbaa003ce5036052c2d49c2cb0d97
Aezore <silverdisc@gmail.com>
2015-02-18 18:46:54 +0100

    a bit of cleanup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 __author__ = 'Research - Jose Rodriguez'
+
+# Import statements
+############################################
 import socket
-
 import kivy
-
 
 kivy.require('1.0.7')
 
@@ -13,35 +14,21 @@ from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
-server_address = ('192.168.0.193', 55555)
-raspi_address = ('192.168.0.197', 55554)
+# Network Settings
+############################################
+server_address = ('192.168.0.193', 55555)  # This computer's IP address and port
+raspi_address = ('192.168.0.197', 55554)  # Raspberry Pi's address and port
 
-socket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # bind server
-socket1.bind(server_address)
+socket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # bind server (this computer) to receive packets
+socket1.bind(server_address)  #
 
-socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create raspi send command bind
+socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create another socket that points to the raspi
 
-
-# data, addr = socket1.recvfrom(1024)
-#strvalue = data.decode('utf-8')
+# Main Program
+############################################
 
 
 class TimeFetch(BoxLayout):
-
-    #text_label = StringProperty("Tiempo:")
-
-    # def __init__(self, **kwargs):
-    #     super(TimeFetch, self).__init__(**kwargs)
-    #
-    #     self.cols = 2
-    #
-    #     tiempo = Label(text=text_label)
-    #     self.add_widget(tiempo)
-    #
-    #     boton = Button(text="Fetch!")
-    #     boton.bind(on_press=self.on_event)
-    #     self.add_widget(boton)
-
     def update_time(self, *args):
         print("Fetching time: ")
         comando = 20
@@ -54,23 +41,7 @@ class TimeFetch(BoxLayout):
 class RigMK2core(App):
     def build(self):
         return TimeFetch()
-        #tiempo = TimeFetch()
-        #return tiempo
 
 
 if __name__ == '__main__':
     RigMK2core().run()
-
-
-
-
-
-
-
-# Old code
-#-----------------------
-# while True:
-# data, addr = socket1.recvfrom(1024)
-#    # button1 = Button(size_hint=(None, None), text='plop')
-#    strvalue = data.decode('utf-8')
-#    print("Received: ", strvalue)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rigmk2core',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Research',
+    author_email='',
+    description=''
+)
